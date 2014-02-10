725bb5561b9ca4be14568ea55315601ef65be937
Scott Burton <spurton@gmail.com>
2014-02-10 15:06:44 -0800

    Generic auth

--- app/controllers/redis_node_view/application_controller.rb
+++ app/controllers/redis_node_view/application_controller.rb
@@ -1,4 +1,17 @@
+require 'will_paginate/array'
+
 module RedisNodeView
   class ApplicationController < ActionController::Base
+    protect_from_forgery
+    before_filter :auth
+
+    private
+
+    def auth
+      if ENV["REDIS_NODE_VIEW_HTTP_BASIC_AUTH_USER"] && ENV["REDIS_NODE_VIEW_HTTP_BASIC_AUTH_PASSWORD"]
+        authenticate_or_request_with_http_basic {|u, p| u == ENV["REDIS_NODE_VIEW_HTTP_BASIC_AUTH_USER"] && p == ENV["REDIS_NODE_VIEW_HTTP_BASIC_AUTH_PASSWORD"] }
+      end
+    end
+
   end
 end
